ce8e10ced9089b99715a6540d954f5052d517c0f
Nolan Seokane <nolan@thedigitalacademy.co.za>
2021-05-14 11:51:50 +0200

    Adding Redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 // Components
-import Home from './components/Home';
 
 function App() {
 	return (
 		<div>
-			<Home />
+			<h1>Hello React App</h1>
 		</div>
 	)
 }
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
 
-import "./index.css";
+import "./index.scss";
 import App from "./App";
 
 ReactDOM.render( <App /> , document.getElementById('root'));
